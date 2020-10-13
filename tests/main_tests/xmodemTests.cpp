7cdc6922b03b621bc6ac580e9e45c791b092b7f4
Nick Brook <nrbrook@gmail.com>
2020-10-13 12:26:43 +0100

    Added tests for loss and corruption

--- tests/main_tests/xmodemTests.cpp
+++ tests/main_tests/xmodemTests.cpp
@@ -15,6 +15,7 @@
 #endif
 
 #define WIRE_BUFFER_SIZE 300
+#define XMODEM_BUFFER_SIZE 128
 static uint8_t tx_wireBuffer[WIRE_BUFFER_SIZE];
 static size_t tx_wireBufferStart = 0;
 static size_t tx_wireBufferEnd = 0;
@@ -22,6 +23,8 @@ static uint8_t rx_wireBuffer[WIRE_BUFFER_SIZE];
 static size_t rx_wireBufferStart = 0;
 static size_t rx_wireBufferEnd = 0;
 static pthread_t sendThread, receiveThread;
+static double lossRate = 0;
+static double corruptionRate = 0;
 int xmodem_InByte(unsigned short timeout) {
     bool isSend = pthread_self() == sendThread;
     uint8_t *wireBuffer = isSend ? rx_wireBuffer : tx_wireBuffer;
@@ -46,16 +49,35 @@ int xmodem_InByte(unsigned short timeout) {
     PRINT_WIRE("%s op failed to receive byte after delay %dms\n", isSend ? "T" : "R", timeout);
     return -10;
 }
+static bool isUnlucky(double rate) {
+    uint32_t r = (uint32_t)random();
+    return r < (uint32_t)((double)UINT32_MAX) * rate;
+}
 void xmodem_OutByte(unsigned char c) {
     bool isSend = pthread_self() == sendThread;
     uint8_t *wireBuffer = isSend ? tx_wireBuffer : rx_wireBuffer;
     size_t *wireBufferEnd = isSend ? &tx_wireBufferEnd : &rx_wireBufferEnd;
-    PRINT_WIRE("%s op T byte 0x%02X\n", isSend ? "T" : "R", (unsigned char)c);
-    wireBuffer[*wireBufferEnd] = (unsigned char)c;
-    *wireBufferEnd += 1;
-    if(*wireBufferEnd == WIRE_BUFFER_SIZE) {
-        *wireBufferEnd = 0;
+    bool corrupt = false, lost = false;
+    if(lossRate > 0) {
+        lost = isUnlucky(lossRate);
     }
+    if(corruptionRate > 0) {
+        corrupt = isUnlucky(corruptionRate);
+    }
+    if(lost) {
+        PRINT_WIRE("%s op T byte 0x%02X LOST\n", isSend ? "T" : "R", c);
+    } else {
+        if(corrupt) {
+            c = (unsigned char)random();
+        }
+        PRINT_WIRE("%s op T byte 0x%02X%s\n", isSend ? "T" : "R", c, corrupt ? "" : " CORRUPT");
+        wireBuffer[*wireBufferEnd] = c;
+        *wireBufferEnd += 1;
+        if(*wireBufferEnd == WIRE_BUFFER_SIZE) {
+            *wireBufferEnd = 0;
+        }
+    }
+
 }
 
 static uint8_t dataToTransfer[WIRE_BUFFER_SIZE];
@@ -109,6 +131,7 @@ class xmodemTests : public ::testing::Test {
         tx_wireBufferEnd = 0;
         rx_wireBufferStart = 0;
         rx_wireBufferEnd = 0;
+        lossRate = 0;
         for(int i = 0; i < sizeof(dataToTransfer); i++) {
             dataToTransfer[i] = (uint8_t)i;
         }
@@ -210,13 +233,74 @@ TEST_F(xmodemTests, testSendReceiveFailBufferFull) {
 }
 
 TEST_F(xmodemTests, testSendReceiveSuccessXmodemFullBufferEnd0) {
-    sendDataSize = 128;
-    dataToTransfer[125] = 0;
-    dataToTransfer[126] = 0;
-    dataToTransfer[127] = 0;
+    sendDataSize = XMODEM_BUFFER_SIZE;
+    dataToTransfer[XMODEM_BUFFER_SIZE-3] = 0;
+    dataToTransfer[XMODEM_BUFFER_SIZE-2] = 0;
+    dataToTransfer[XMODEM_BUFFER_SIZE-1] = 0;
     sendBufferSize = sizeof(dataToTransfer);
     receiveBufferSize = sizeof(receiveBuffer);
     receiveTotalSize = sizeof(receiveBuffer);
+    ::pthread_create(&sendThread, nullptr, sendFunc, nullptr);
+    ::pthread_create(&receiveThread, nullptr, receiveFunc, nullptr);
+    ::pthread_join(sendThread, nullptr);
+    ::pthread_join(receiveThread, nullptr);
+
+    ASSERT_EQ(sendResult, sendDataSize);
+    ASSERT_EQ(receiveResult, sendDataSize);
+    if(receiveResult > receiveOffset) {
+        memcpy(receiveOutput + receiveOffset, receiveBuffer, receiveResult - receiveOffset);
+    }
+    ASSERT_EQ(memcmp(receiveOutput, dataToTransfer, sendDataSize), 0);
+}
+
+TEST_F(xmodemTests, testSendReceiveSuccessFullBufferFullDataLoss) {
+    lossRate = 1.0 / (2.0 * 128.0);
+    sendDataSize = sizeof(dataToTransfer);
+    sendBufferSize = sizeof(dataToTransfer);
+    receiveBufferSize = sizeof(receiveBuffer);
+    receiveTotalSize = sizeof(receiveBuffer);
+
+    ::pthread_create(&sendThread, nullptr, sendFunc, nullptr);
+    ::pthread_create(&receiveThread, nullptr, receiveFunc, nullptr);
+    ::pthread_join(sendThread, nullptr);
+    ::pthread_join(receiveThread, nullptr);
+
+    ASSERT_EQ(sendResult, sendDataSize);
+    ASSERT_EQ(receiveResult, sendDataSize);
+    if(receiveResult > receiveOffset) {
+        memcpy(receiveOutput + receiveOffset, receiveBuffer, receiveResult - receiveOffset);
+    }
+    ASSERT_EQ(memcmp(receiveOutput, dataToTransfer, sendDataSize), 0);
+}
+
+TEST_F(xmodemTests, testSendReceiveSuccessFullBufferFullDataCorruption) {
+    corruptionRate = 1.0 / (2.0 * 128.0);
+    sendDataSize = sizeof(dataToTransfer);
+    sendBufferSize = sizeof(dataToTransfer);
+    receiveBufferSize = sizeof(receiveBuffer);
+    receiveTotalSize = sizeof(receiveBuffer);
+
+    ::pthread_create(&sendThread, nullptr, sendFunc, nullptr);
+    ::pthread_create(&receiveThread, nullptr, receiveFunc, nullptr);
+    ::pthread_join(sendThread, nullptr);
+    ::pthread_join(receiveThread, nullptr);
+
+    ASSERT_EQ(sendResult, sendDataSize);
+    ASSERT_EQ(receiveResult, sendDataSize);
+    if(receiveResult > receiveOffset) {
+        memcpy(receiveOutput + receiveOffset, receiveBuffer, receiveResult - receiveOffset);
+    }
+    ASSERT_EQ(memcmp(receiveOutput, dataToTransfer, sendDataSize), 0);
+}
+
+TEST_F(xmodemTests, testSendReceiveSuccessFullBufferFullDataLossAndCorruption) {
+    lossRate = 1.0 / (3.0 * 128.0);
+    corruptionRate = 1.0 / (3.0 * 128.0);
+    sendDataSize = sizeof(dataToTransfer);
+    sendBufferSize = sizeof(dataToTransfer);
+    receiveBufferSize = sizeof(receiveBuffer);
+    receiveTotalSize = sizeof(receiveBuffer);
+
     ::pthread_create(&sendThread, nullptr, sendFunc, nullptr);
     ::pthread_create(&receiveThread, nullptr, receiveFunc, nullptr);
     ::pthread_join(sendThread, nullptr);
